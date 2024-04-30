a121a8795f06fbc54fe1819b0da00a8241da80f6
renatochocce <renatochocce758@gmail.com>
2024-04-30 08:56:10 -0600

    hkgkhgk

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ let contadorElemento = document.querySelector("#contador");
 let contador = 0;
 
 function decrementarButton() {
-    if (contador > 0) { // si contador es mayor a cero va a decrementar
-        contador--;
-    }else {
-        console.log("no se pueden decrementar numeros negativos")
-    }
+    // if (contador > 0) { // si contador es mayor a cero va a decrementar
+    //     contador--;
+    // }else {
+    //     console.log("no se pueden decrementar numeros negativos")
+    // }
+
+    contador--;
     contadorElemento.textContent = contador;
     console.log(contador);
 
